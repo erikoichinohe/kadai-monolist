84dc489efa7940da7a974750a7eaea790ce13d23
Eriko <lo_oleriko@hotmail.com>
2017-02-21 08:04:58 +0000

    have

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\User;
+use App\Item;
 
 class UsersController extends Controller
 {
@@ -50,6 +52,7 @@ class UsersController extends Controller
     {
         $user = User::find($id);
         $count_want = $user->want_items()->count();
+        $count_have = $user->have_items()->count();
         $items = [];
          if (Item::exists()) {
             $items = \DB::table('items')->join('item_user', 'items.id', '=', 'item_user.item_id')->select('items.*')->where('item_user.user_id', $user->id)->distinct()->groupBy('items.id')->paginate(20);
