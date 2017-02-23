aab22a1db87d57f1c1a455c662b78200379b30ff
Eriko <lo_oleriko@hotmail.com>
2017-02-23 07:35:48 +0000

    want ranking

--- app/Http/Controllers/RankingController.php
+++ app/Http/Controllers/RankingController.php
@@ -8,8 +8,19 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\Item;
 
-class ItemsController extends Controller
+class RankingController extends Controller
 {
+    public function want()
+    {
+        $items=[];
+        if(Item::exists()){
+            $items = \DB::table('item_user')->join('items', 'item_user.item_id', '=', 'items.id')->select('items.*', \DB::raw('COUNT(*) as count'))->where('type', 'want')->groupBy('items.id')->orderBy('count', 'DESC')->take(10)->get();
+        }
+        return view('ranking.want', [
+            'items' => $items,
+        ]);
+    }
+    
     /**
      * Display a listing of the resource.
      *
@@ -27,33 +38,7 @@ class ItemsController extends Controller
      */
     public function create()
     {
-        $keyword = request()->keyword;
-        $items = [];
-        
-        if($keyword){
-            $client = new \RakutenRws_Client();
-            $client->setApplicationId(env('RAKUTEN_APPLICATION_ID'));
-            
-            $rws_response = $client->execute('IchibaItemSearch', [
-                'keyword' => $keyword,
-                'imageFlag' => 1,
-                'hits' => 20,
-            ]);
-            // 扱い易いように Item としてインスタンスを作成する（保存はしない）
-            foreach ($rws_response->getData()['Items'] as $rws_item) {
-                $item = new Item();
-                $item->code = $rws_item['Item']['itemCode'];
-                $item->name = $rws_item['Item']['itemName'];
-                $item->url = $rws_item['Item']['itemUrl'];
-                $item->image_url = str_replace('?_ex=128x128', '', $rws_item['Item']['mediumImageUrls'][0]['imageUrl']);
-                $items[] = $item;
-            }
-        }
-        
-        return view('items.create',[
-            'keyword' => $keyword,
-            'items' => $items,
-        ]);
+        //
     }
 
     /**
@@ -75,15 +60,7 @@ class ItemsController extends Controller
      */
     public function show($id)
     {
-        $item = Item::find($id);
-        $want_users = $item->want_users;
-        $have_users = $item->have_users;
-
-        return view('items.show', [
-          'item' => $item,
-          'want_users' => $want_users,
-          'have_users' => $have_users,
-        ]);
+        //
     }
 
     /**
--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -1,6 +1,6 @@
 @if($items)
     <div class="row">
-        @foreach($items as $item)
+        @foreach($items as $key => $item)
         <div class="item">
             <div class="col-md-3 col-sm-4 col-xs-12">
                 <div class="panel panel-default">
@@ -9,13 +9,20 @@
                     </div>
                     <div class="panel-body">
                         <p class="item-title"><a href="{{ route('items.show', $item->id) }}">{{ $item->name }}</a></p>
+                    
+                        <div class="buttons text-center">
+                            @if (Auth::check())
+                            @include('items.want_button', ['item' => $item])
+                            @include('items.have_button', ['item' => $item])
+                            @endif
+                        </div>
                     </div>
-                    <div class="buttons text-center">
-                        @if (Auth::check())
-                        @include('items.want_button', ['item' => $item])
-                        @include('items.have_button', ['item' => $item])
-                        @endif
-                    </div>
+                        
+                    @if (isset($item->count))
+                        <div class="panel-footer">
+                            <p class="text-center">{{ $key+1 }}位: {{ $item->count}} Wants</p>
+                        </div>
+                    @endif
                 </div>
             </div>
         </div>
